14d6624dbec8d90d106f89b71ff76c3a23c5e806
Marina Vyatkina <marinavyatk74@mail.ru>
2024-04-23 02:07:08 +0400

    Added caseCircle + filterButton

---
+++ src/components/ui/complex/caseCircleList/caseCircleList.tsx
@@ -0,0 +1,18 @@
+import {ComponentPropsWithoutRef} from 'react';
+import clsx from 'clsx';
+import s from './caseCircleList.module.scss'
+import {CaseCircle} from '../../primitive/caseCircle/caseCircle.tsx';
+
+export type CaseCircleProps = { caseCircles: CaseCircle[] } & ComponentPropsWithoutRef<'div'>
+
+export const CaseCircleList = (props: CaseCircleProps) => {
+    const {caseCircles, className, ...restProps} = props;
+    const classNames = clsx(s.circles, className)
+    const CaseCircles = caseCircles.map(circle => {
+        return <CaseCircle img={circle.img} caseId={circle.caseId} category={circle.category} key={circle.caseId}/>
+    })
+
+    return <div  {...restProps} className={classNames}>
+        {CaseCircles}
+    </div>
+}
