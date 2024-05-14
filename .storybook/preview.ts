6b98cd69f17529fafa6ec8f92c7629637198855f
Marina Vyatkina <marinavyatk74@mail.ru>
2024-05-15 00:17:29 +0400

    Added storybook dark theme

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,9 +2,22 @@ import type { Preview } from "@storybook/react";
 import "@fontsource-variable/unbounded";
 import "@fontsource-variable/montserrat";
 import "../src/styles/index.scss";
+import { themes } from "@storybook/theming";
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
+    docs: {
+      theme: themes.dark,
+    },
+    backgrounds: {
+      default: "dark",
+      values: [
+        {
+          name: "dark",
+          value: "#000000",
+        },
+      ],
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
