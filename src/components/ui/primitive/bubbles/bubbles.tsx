8e168ea05d4e73d8a51b1535e55d4337c5cf002b
Marina Vyatkina <marinavyatk74@mail.ru>
2024-07-22 18:18:53 +0400

    Adapted 768px -header -about

--- src/components/ui/primitive/bubbles/bubbles.tsx
+++ src/components/ui/primitive/bubbles/bubbles.tsx
@@ -1,11 +1,3 @@
-import Spline from "@splinetool/react-spline";
-import s from "./bubbles.module.scss";
-
 export const MainPageBubbles = () => {
-  return (
-    <Spline
-      scene="https://draft.spline.design/2LpYncAHxqm6Gdk3/scene.splinecode"
-      className={s.bubbles}
-    />
-  );
+  return <div />;
 };
