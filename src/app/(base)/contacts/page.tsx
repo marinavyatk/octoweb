7086b5097af5f4331512dc58eec01150845c46fd
Marina Vyatkina <marinavyatk74@mail.ru>
2024-11-11 12:48:41 +0400

    Adaptive for contacts page

--- src/app/(base)/contacts/page.tsx
+++ src/app/(base)/contacts/page.tsx
@@ -1,25 +1,26 @@
-import s from './contacts.module.scss';
-import Map from '@/components/layouts/map/map';
+import s from "./contacts.module.scss";
+import Map from "@/components/layouts/map/map";
 
-export default function Contacts (){
+export default function Contacts() {
   return (
     <div className={s.contactsPage}>
-      <div className={s.contactsPageContent}>
-        <div className={s.mainContainer}>
-          <h1>КОНТАКТЫ </h1>
-          <div className={s.contacts}>
-            <p>
-              Пн-пт 10:00 — 18:00. <br />
-              г. Краснодар, <span>ул. Атарбекова 7</span>
-            </p>
-            <p>
-              info@octoweb.ru <br /> +7 905 407-78-32
-            </p>
+      <div className={"mainContainer"}>
+        <h1>КОНТАКТЫ </h1>
+        <div className={s.contacts}>
+          <div>
+            Пн-пт 10:00 — 18:00. <br />
+            <address>
+              г. Краснодар, ул.&nbsp;Атарбекова&nbsp;7
+            </address>
+          </div>
+          <div className={s.links}>
+            <a href="mailto:info@octoweb.ru">info@octoweb.ru</a>
+            <a href="tel:+79054077832">+7 905 407-78-32</a>
           </div>
         </div>
-        <div className={s.map}>
-          <Map />
-        </div>
+      </div>
+      <div className={s.map}>
+        <Map />
       </div>
     </div>
   );
