827b9707b5a618bf035cf3bbe31c8c425abeeca8
Marina Vyatkina <marinavyatk74@mail.ru>
2024-12-04 17:38:15 +0400

    Remove 3d

--- src/app/(base)/page.tsx
+++ src/app/(base)/page.tsx
@@ -11,13 +11,6 @@ import { Button } from "@/components/ui/buttons/button/button";
 import Link from "next/link";
 import { Greeting } from "@/components/sections/greeting/greeting";
 import { linksData } from "@/common/componentsData/servicesLinks";
-import {
-  BigBubbleScene,
-  BigBubbleSceneLeft,
-  Scene,
-  SmallBubbleScene,
-  SmallBubbleSceneRight
-} from "@/components/3d/scene/scene";
 
 
 export default function Home() {
@@ -30,8 +23,6 @@ export default function Home() {
 
   return (
     <div className={s.mainPage}>
-      <Scene/>
-      <BigBubbleScene/>
       <div className={"mainContainer"}>
         <Greeting textContent={content} />
         <h1 className={s.hiddenHeader}>Создаем сайты для бизнеса</h1>
@@ -41,8 +32,6 @@ export default function Home() {
       <div className={"mainContainer"}>
         <section className={s.advantages}>
           <h2>ПОЧЕМУ МЫ?</h2>
-          <BigBubbleSceneLeft/>
-          <SmallBubbleScene/>
           <div className={s.arrow}></div>
           <span className={s.backgroundSymbol}>くコ:彡</span>
           <span className={s.backgroundSymbol}>くコ:彡</span>
@@ -67,15 +56,10 @@ export default function Home() {
             className={s.fourCard}
           />
           <Button as={Link} text={"Больше кейсов"} href={routes.cases} className={s.arrowLink} />
-          <SmallBubbleSceneRight/>
         </section>
       </div>
       <ServicesLinksList linksData={linksData} />
-      <div className={s.bubbleContainer}>
-        <BigBubbleSceneLeft/>
-      </div>
-
-      <StepCards className={s.steps} />
+       <StepCards className={s.steps} />
     </div>
   );
 };
