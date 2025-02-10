c8aa5b652dc3082e32dab137b4e104cabe47af46
Marina Vyatkina <marinavyatk74@mail.ru>
2025-02-10 11:03:05 +0400

    Changed wordSwipe, date in footer, arrow animation, step cards

--- src/components/sections/stepCards/stepCards.stories.ts
+++ src/components/sections/stepCards/stepCards.stories.ts
@@ -11,4 +11,54 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {};
+
+const stepCards = [
+  {
+    stepNumber: "01",
+    header: "Первый контакт",
+    description:
+      "Знакомимся с вами, оцениваем цели и задачи проекта, составляем техническое задание"
+  },
+  {
+    stepNumber: "02",
+    header: "Анализ Вашей ниши",
+    description:
+      "Изучаем конкурентов и ЦА, на основе полученных данных формируем КП"
+  },
+  {
+    stepNumber: "03",
+    header: "Подписание договора",
+    description:
+      "На основе КП и ТЗ мы составляем договор и проводим его подписание"
+  },
+  {
+    stepNumber: "04",
+    header: "Создание прототипа",
+    description:
+      "Разрабатываем прототип будущего сайта с низкой детализацией на основе мудборда"
+  },
+  {
+    stepNumber: "05",
+    header: "Разработка дизайна",
+    description:
+      "Подготавливаем дизайн-проект в Figma, при необходимости вносим правки"
+  },
+  {
+    stepNumber: "06",
+    header: "Верстка и посадка",
+    description:
+      "Верстаем, проводим адаптацию под все разрешения, интегрируем готовую версту в CMS"
+  },
+  {
+    stepNumber: "07",
+    header: "Сдача проекта",
+    description:
+      "Презентуем готовый сайт, вносим финальные правки и подписываем закрывающие документы"
+  }
+];
+
+export const Primary: Story = {
+  args: {
+    stepCards: stepCards
+  }
+};
