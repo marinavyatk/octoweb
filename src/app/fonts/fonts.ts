fde1e61724cb1c9cb236aed0afe371a824aa8e96
Marina Vyatkina <marinavyatk74@mail.ru>
2025-02-24 21:59:17 +0400

    Removed unused font settings

--- src/app/fonts/fonts.ts
+++ src/app/fonts/fonts.ts
@@ -3,15 +3,11 @@ import {Montserrat, Unbounded} from 'next/font/google';
 export const unbounded = Unbounded({
     subsets: ['cyrillic'],
     variable: '--unbounded',
-    // display: 'auto',
-    display: 'swap',
+    display: 'auto',
     weight: ["200", "300", "400", "500", "600", "800", "900" ],
-    fallback: ['sans-serif','Calibri', 'Arial']
 });
 export const montserrat = Montserrat({
     subsets: ['cyrillic'],
     variable: '--montserrat',
-    display: 'swap',
     weight: ["300", "500" ],
-    fallback: ['sans-serif', 'Arial', 'Calibri']
 })
