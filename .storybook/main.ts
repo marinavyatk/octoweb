8492cb06f9f7cc0bd9761994c82f543632c91910
Marina Vyatkina <marinavyatk74@mail.ru>
2025-03-03 23:42:46 +0400

    Storybook config change

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -29,6 +29,7 @@ const config: StorybookConfig = {
     config.resolve = config.resolve || {};
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
+      "@": path.resolve(__dirname, "../src"),
       public: path.resolve(__dirname, "../public"),
     };
 
