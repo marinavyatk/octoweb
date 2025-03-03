e08832f0cedb68a3373f26ba251d9f07823e4865
Marina Vyatkina <marinavyatk74@mail.ru>
2025-03-03 12:48:21 +0400

    Added word

--- src/app/(base)/page.tsx
+++ src/app/(base)/page.tsx
@@ -65,7 +65,7 @@ export default async function Home() {
     firstLine: "МЫ РЕВОЛЮЦИОНЕРЫ",
     secondLine: "В СФЕРЕ",
     thirdLine: "ИЗ КРАСНОДАРА",
-    wordSwipeProps: { words: ["web", "seo"] },
+    wordSwipeProps: { words: ["web", "seo", "ads"] },
   };
 
   const schema = seo?.schema;
