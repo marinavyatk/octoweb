4f4ad2c82d9f07e4331de0460b6b16ceee0ce80c
Marina Vyatkina <marinavyatk74@mail.ru>
2025-03-06 20:45:38 +0400

    Check

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
 }: Readonly<{
   children: ReactNode;
 }>) {
-  console.log("Push check 2");
+  console.log("Push check 3");
   return (
     <html lang="ru" className={clsx(unbounded.variable, montserrat.variable)}>
       <head>
